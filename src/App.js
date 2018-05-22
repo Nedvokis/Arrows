319ce56bf2ab6fd5b06b6c8905cae4cfd9931135
Denis Hilkov <d.khilkov@gmail.com>
2018-05-22 16:24:18 +0300

    add page to route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { Provider } from 'react-redux';
 import createStore from './globalRedux';
 import { BrowserRouter, Switch, Route} from 'react-router-dom';
 import Home from './pages/Home/Home';
-import UserProfile from './pages/UserProfile/UserProfile';
+import GroupPage from './pages/GroupPage/GroupPage';
+import UserPage from './pages/UserPage/UserPage';
 import './App.css';
 
 const store = createStore();
@@ -13,7 +14,8 @@ const App = () => (
     <BrowserRouter>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/account" component={UserProfile} />
+        <Route exact path="/user" component={UserPage} />
+        <Route exact path="/group" component={GroupPage} />
       </Switch>
     </BrowserRouter>
   </Provider>
