4f4ec508e8f3c0563224cbe893221aeadb4ef982
Denis Khilkov <d.khilkov@gmail.com>
2019-01-24 11:35:11 +0300

    make ListGroups to show groups from object of ListGroups

--- src/constants/devMeta.js
+++ src/constants/devMeta.js
@@ -3,37 +3,37 @@ export const LIST_OF_GROUPS = [
     title: 'HTML 5 и его полезности.',
     time: '34.05.2018 в 14:00',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\n' +
-    '            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n' +
-    '            Ut enim ad minim veniam,\n' +
-    '            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n' +
-    '            Duis aute irure dolor in reprehenderit in voluptate velit...',
+      '            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n' +
+      '            Ut enim ad minim veniam,\n' +
+      '            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n' +
+      '            Duis aute irure dolor in reprehenderit in voluptate velit...',
+  },
+  {
+    title: 'JavaScript ESover9000 LUL',
+    time: '34.05.2018 в 14:00',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\n' +
+      '            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n' +
+      '            Ut enim ad minim veniam,\n' +
+      '            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n' +
+      '            Duis aute irure dolor in reprehenderit in voluptate velit...',
   },
   {
     title: 'HTML 5 и его полезности.',
     time: '34.05.2018 в 14:00',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\n' +
-    '            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n' +
-    '            Ut enim ad minim veniam,\n' +
-    '            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n' +
-    '            Duis aute irure dolor in reprehenderit in voluptate velit...',
+      '            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n' +
+      '            Ut enim ad minim veniam,\n' +
+      '            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n' +
+      '            Duis aute irure dolor in reprehenderit in voluptate velit...',
   },
   {
     title: 'HTML 5 и его полезности.',
     time: '34.05.2018 в 14:00',
     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\n' +
-    '            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n' +
-    '            Ut enim ad minim veniam,\n' +
-    '            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n' +
-    '            Duis aute irure dolor in reprehenderit in voluptate velit...',
-  },
-  {
-    title: 'HTML 5 и его полезности.',
-    time: '34.05.2018 в 14:00',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,\n' +
-    '            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n' +
-    '            Ut enim ad minim veniam,\n' +
-    '            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n' +
-    '            Duis aute irure dolor in reprehenderit in voluptate velit...',
+      '            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n' +
+      '            Ut enim ad minim veniam,\n' +
+      '            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n' +
+      '            Duis aute irure dolor in reprehenderit in voluptate velit...',
   },
 ];
 
--- src/containers/ListGroups/ListGroups.js
+++ src/containers/ListGroups/ListGroups.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import ListGroupItem from 'components/ListGroupItem/ListGroupItem'
+import ListGroupItem from 'components/ListGroupItem/ListGroupItem';
+import { LIST_OF_GROUPS } from 'constants/devMeta';
 import './ListGroups.scss';
 
 class ListGroups extends Component {
   render() {
-    return(
+    return (
       <div className="list-groups">
         <div className="container">
-          <ListGroupItem />
+          {LIST_OF_GROUPS.map(group => (
+            <ListGroupItem title={group.title} />
+          ))}
         </div>
       </div>
     );
